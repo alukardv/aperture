d7ca9b918d225cf7c1f991dc5bc5418f5ba1ae7f
alukardv <semenovnsu@gmail.com>
2024-02-16 00:06:35 +0200

    zd 13

--- js/scripts.js
+++ js/scripts.js
@@ -11,16 +11,55 @@ $('.owl-carousel').owlCarousel({
     }
 });
 
-var menuBtn = document.querySelector('.menu-btn');
-var nav = document.querySelector('nav');
-var lineOne = document.querySelector('nav .menu-btn .line--1');
-var lineTwo = document.querySelector('nav .menu-btn .line--2');
-var lineThree = document.querySelector('nav .menu-btn .line--3');
-var link = document.querySelector('nav .nav-links');
-menuBtn.addEventListener('click', () => {
-    nav.classList.toggle('nav-open');
-    lineOne.classList.toggle('line-cross');
-    lineTwo.classList.toggle('line-fade-out');
-    lineThree.classList.toggle('line-cross');
-    link.classList.toggle('fade-in');
-})
+
+jQuery(document).ready(function($){
+    //toggle 3d navigation
+    $('.cd-3d-nav-trigger').on('click', function(){
+        toggle3dBlock(!$('.cd-header').hasClass('nav-is-visible'));
+    });
+
+    //select a new item from the 3d navigation
+    $('.cd-3d-nav a').on('click', function(){
+        var selected = $(this);
+        selected.parent('li').addClass('cd-selected').siblings('li').removeClass('cd-selected');
+        updateSelectedNav('close');
+    });
+
+    $(window).on('resize', function(){
+        window.requestAnimationFrame(updateSelectedNav);
+    });
+
+    function toggle3dBlock(addOrRemove) {
+        if(typeof(addOrRemove)==='undefined') addOrRemove = true;
+        $('.cd-header').toggleClass('nav-is-visible', addOrRemove);
+        $('main').toggleClass('nav-is-visible', addOrRemove);
+        $('.cd-3d-nav-container').toggleClass('nav-is-visible', addOrRemove);
+    }
+
+    //this function update the .cd-marker position
+    function updateSelectedNav(type) {
+        var selectedItem = $('.cd-selected'),
+            selectedItemPosition = selectedItem.index() + 1,
+            leftPosition = selectedItem.offset().left,
+            backgroundColor = selectedItem.data('color');
+
+        $('.cd-marker').removeClassPrefix('color').addClass('color-'+ selectedItemPosition).css({
+            'left': leftPosition,
+        });
+        if( type == 'close') {
+            $('.cd-marker').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+                toggle3dBlock(false);
+            });
+        }
+    }
+
+    $.fn.removeClassPrefix = function(prefix) {
+        this.each(function(i, el) {
+            var classes = el.className.split(" ").filter(function(c) {
+                return c.lastIndexOf(prefix, 0) !== 0;
+            });
+            el.className = $.trim(classes.join(" "));
+        });
+        return this;
+    };
+});
